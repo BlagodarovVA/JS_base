e2155054340fc505bbcdb0852fd07e7d8395788f
Valery <11w-walerik@mail.ru>
2022-05-20 15:21:59 +0300

    finish 20

---
+++ JS20/script.js
@@ -0,0 +1,36 @@
+'use strict';
+
+if (4) {
+    console.log ('Ok!');
+} else {
+    console.log ('Error');
+}
+
+
+const num = 50;
+if (num < 49) {
+    console.log ('Error');
+} else if (num > 100) {
+    console.log ('Много');
+} else {
+    console.log ('Ok');
+}
+
+
+const num1 = 50;
+//(num1 === 50) ? console.log ('Okay') : console.log ('Error');
+
+switch (num1) {
+    case 49:
+        console.log ('Неверно');
+        break;
+    case 100:
+        console.log ('Неверно');
+        break;
+    case 51:
+        console.log ('Good Waldemar!');
+        break;                
+    default:                            //если не один кейз не подошёл
+        console.log ('default');
+        break;            
+}
