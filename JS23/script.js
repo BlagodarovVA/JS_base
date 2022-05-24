17d95139f3fc1fa1e658ed745ebecde824fe9fae
Valery <11w-walerik@mail.ru>
2022-05-24 14:20:06 +0300

    23 done

---
+++ JS23/script.js
@@ -0,0 +1,78 @@
+'use strict';
+
+for (let i = 0; i < 3; i++) {
+    console.log(i);
+    for (let j = 10; j < 13; j++) {
+        console.log(j);
+    }
+}
+
+//*
+//**
+//***
+//****
+//*****
+//******
+
+let result = '';
+const leng = 7;
+for (let i = 1; i < leng; i++) {
+    for (let j = 0; j < i; j++) {
+        result += "*";
+    }
+    result += '\n';
+}
+console.log(result);
+
+
+first: for (let i = 0; i < 3; i++) {
+    console.log(`First level: ${i}`);
+    for (let j = 0; j < 3; j++) {
+        console.log(`Second level: ${j}`);
+        for (let k = 0; k < 3; k++) {
+            if (k === 2) { 
+                continue first;
+            }
+            console.log(`Third level: ${k}`);
+        }
+    }
+}
+//task 1
+for (let i = 5; i < 11; i++) {
+    console.log(i);
+}
+
+//task 2
+for (let i = 20; i > 9; i--) {
+    if (i === 13) {
+        break;
+    }
+    console.log(i);
+}
+
+//task 3
+for (let i = 2; i < 11; i++) {
+    if ( i % 2 === 0) {
+        console.log(i);
+    } 
+}
+
+//task 4
+let i = 2;
+while (i < 16) {
+    if (i % 2 !== 0) {
+        console.log(i);
+    }    
+    i++;
+}
+
+// task 5
+const arrayOfNumbers = [];
+let i = 5;
+let j = 0;
+while (i < 11) {
+    arrayOfNumbers[j] = i;
+    i++;
+    j++;
+}
+console.log(arrayOfNumbers);
