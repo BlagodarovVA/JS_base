d07843cd8d4f02c16f031b36e890c232a50552b9
Valery <11w-walerik@mail.ru>
2022-06-14 17:43:51 +0300

    36 done

--- JS36/script.js
+++ JS36/script.js
@@ -3,5 +3,34 @@
 let str = "some";
 let strObj = new String(str);
 
-console.log(typeof(str));
-console.log(typeof(strObj));
+//console.log(typeof(str));
+//console.log(typeof(strObj));
+
+console.dir([1,2,3]);
+
+const soldier = {
+    health: 400,
+    armor: 100,
+    sayHello: function() {
+        console.log('Hello');
+    }
+};
+
+/*const john = {
+    health: 100
+};*/
+
+//создаем новый объект, который будет прототипно наследоваться от солдата
+const john = Object.create(soldier);
+
+//устаревший формат установки прототипа
+//john.__proto__ = soldier;
+
+//console.log(john.armor);
+
+//новый формат установки прототипа
+//Object.setPrototypeOf(john, soldier);
+
+//метод в прототипе
+john.sayHello();
+
