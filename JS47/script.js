37fe5665d9302302ddf5b61d682d101f2533f536
Валерий Благодаров (домашний ноут) <11w-walerik@mail.ru>
2022-07-10 22:49:04 +0300

    47 done

--- JS47/script.js
+++ JS47/script.js
@@ -1,4 +1,5 @@
-function pow1(x, n) {
+ /*
+ function pow1(x, n) {
     let result = 1;
 
     for (let i = 0; i < n; i++) {
@@ -15,3 +16,91 @@ function pow2(x, n) {
         return x * pow2(x, n - 1);
     }
 }
+*/
+
+let students = {
+    js: [{
+        name: 'John',
+        progress: 100
+    }, {
+        name: 'Ivan',
+        progress: 60
+    }],
+
+    html: {
+        basic: [{
+            name: 'Peter',
+            progress: 20
+        }, {
+            name: 'Ann',
+            progress: 18
+        }],
+
+        pro: [{
+            name: 'Sam',
+            progress: 10
+        }],
+
+        semi: {
+            students: [{
+                name: 'Test',
+                progress: 100
+            }]
+        }
+    }
+};
+
+//перебор структур данных. объекты и массивы без рекурсии
+function getTotalProgressByIteration(data) {
+    let total = 0;
+    let students = 0;
+
+    for (let course of Object.values(data)) {
+        if(Array.isArray(course)) {
+            students += course.length;
+
+            for (let i = 0; i < course.length; i++) {
+                total += course[i].progress;
+            }
+        } else {
+            for (let subCourse of Object.values(course)) {
+                students += subCourse.length;
+
+                for (let i = 0; i < subCourse.length; i++) {
+                    total += subCourse[i].progress;
+                }
+            }
+        }
+    }
+
+    return total / students;
+}
+console.log(getTotalProgressByIteration(students));
+
+
+//перебор структур данных. объекты и массивы рекурсией
+function getTotalProgressByRecursion(data) {
+    if(Array.isArray(data)) {
+        let total = 0;
+
+        for (let i = 0; i < data.length; i++) {
+            total += data[i].progress;
+        }
+        return [total, data.length];
+    } else {
+        let total = [0, 0];
+
+        for (let subData of Object.values(data)) {
+            const subDataArr = getTotalProgressByRecursion(subData);
+            total[0] += subDataArr[0];
+            total[1] += subDataArr[1];
+        }
+
+        return total;
+
+    }
+}
+
+const result = getTotalProgressByRecursion(students);
+console.log(result[0] / result[1]);
+
