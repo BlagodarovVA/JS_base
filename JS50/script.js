9b3702c99e23ef1b15715302bee881aa4b3a3451
Valery <11w-walerik@mail.ru>
2022-07-13 17:28:04 +0300

    50 done

---
+++ JS50/script.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const p = document.querySelectorAll('p');
+console.log(p);
+
+//добавляем тег скрипта в html
+
+function loadScript(src) {
+    const script = document.createElement('script');
+    script.src = src;
+    script.async = false;       //чтобы не был асинхронным
+    document.body.append(script);
+}
+
+loadScript("script1.js");
+loadScript("script2.js");
