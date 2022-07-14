971424a65145d702346316334289c96d82cbb716
Valery <11w-walerik@mail.ru>
2022-07-14 14:50:43 +0300

    53 done

---
+++ JS53/script.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const box = document.querySelector('.box');
+
+const newHeight = 100;
+const newWidth = 400;
+
+function changeParams(elem, h, w) {
+    elem.style.height = `${h ?? 200}px`;    //оператор ИЛИ вернёт первое правдивое значение
+    elem.style.width = `${w ?? 200}px`;     // например аргументы не переданы в стр 13
+    elem.innerHTML = (h ?? 200) * (w ?? 200);   // если существует высота, то умножим на существующую ширину
+}                                               // скобки нужны т.к. разный приоритет операций
+
+changeParams(box, newHeight, newWidth);
+
+let userName;                           //оператор nullish реагирует на null и undefined
+let userKey;
+
+console.log(userName ?? userKey ?? 'User');        //не работает тут, работает в браузере
+                                                    //можно строить цепочку из значений для поиска первого значения
+
