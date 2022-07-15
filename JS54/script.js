caf47b84d3467aea35a8c75b90300ce436cc73e1
Valery <11w-walerik@mail.ru>
2022-07-15 16:20:09 +0300

    54 done

---
+++ JS54/script.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const box = document.querySelector('.box');
+const block = document.querySelector('.block');
+
+console.log(block);
+
+/*if (block) {                              //если блок не существует, то код продолжит выполняться,
+    console.log(block.textContent);         //  а не станет на ошибке
+}
+
+console.log(block?.textContent);            // существует ли блок? если да, то получаем свойство
+                                            // если нет, то получаем undefined и продолжаем
+console.log(1 + 2);
+
+
+block?.textContent = '123';                 // работает на чтение свойства, но при записи получим ошибку
+*/
+
+const userData = {
+    name: 'Fedor',
+    age: null,
+    say: function() {
+        console.log('Hello');
+    }
+};
+
+
+userData.say();
+userData.hey?.();                                              // ?. - проверка, что метод существует
+/* if (userData && userData.skills && userData.skills.js) {    //чтобы не получить ошибку нужен конский if
+    console.log(userData.skills.js);
+}*/
+
+console.log(userData?.skills?.js);           // ? проверит часть слева от себя. если она существует
+                                            // то код пойдет дальше. если нет, то undefined
+
+
+
+
+
+
+
+
+
