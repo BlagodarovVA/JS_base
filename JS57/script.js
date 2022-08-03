1597ac40663cf74ee6782f6ce6cf29866fd51d26
Valery <11w-walerik@mail.ru>
2022-08-03 15:26:50 +0300

    57 done

---
+++ JS57/script.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const user = {
+    name: 'Alex',
+    surname: 'Smith',
+    birthday: '20/04/1990',
+    showMyPublicData: function() {
+        console.log(`${this.name} ${this.surname}`);
+    }
+};
+
+// флаги:
+// writable
+// enumerable
+// configurable
+
+Object.defineProperty(user, 'birthday', {writable: false});         //запрещена запись свойства
+Object.defineProperty(user, 'gender', {value: 'male'});         // свойство создаётся со всеми флагами false
+
+console.log(Object.getOwnPropertyDescriptor(user, 'birthday'));     //просмотр флагов
+console.log(Object.getOwnPropertyDescriptor(Math, 'PI'));           // свойство сделано, как нередактируемое
+
+
+Object.defineProperty(user, 'showMyPublicData', {enumerable: false});   //метод showMyPublicData не увидим в циклах
+
+for (let key in user) console.log(key);            
+
+
+//редактирование нескльких свойств
+Object.defineProperties(user, {
+    name: {writable: false},
+    surname: {writable: false}
+});
+
