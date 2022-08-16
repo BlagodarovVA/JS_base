df45a140f1330ebad676489e01da86c7b425a60b
Valery <11w-walerik@mail.ru>
2022-08-16 17:47:39 +0300

    61-18 done

--- JS61/script18.js
+++ JS61/script18.js
@@ -1,2 +1,27 @@
 'use strict';
 
+//проверить, что в строке есть каждая буква алфавита
+
+function isPangram(string) {
+    let atrAngl1 = 'abcdefghijklmnopqrstuvwxyz';
+    let lengAngl = atrAngl1.length;
+    console.log(lengAngl);
+
+    let str1 = string.replace(/ /g, '');
+    str1.toLowerCase();
+    console.log('str1: ' + str1);
+    let res = true;
+
+    for (let i = 0; i < lengAngl; i++) {
+        if (str1.includes(atrAngl1[i]) == false) {
+            res = false;
+            console.log(atrAngl1[i]);
+            console.log(res);
+            return res;
+        } 
+
+    }
+    return res;
+}
+
+isPangram('Hello world');
