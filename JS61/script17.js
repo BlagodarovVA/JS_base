0dfde3a3cd4fa2db812a1ac46dfe3969e6ae5683
Valery <11w-walerik@mail.ru>
2022-08-16 15:07:28 +0300

    64-17 done

---
+++ JS61/script17.js
@@ -0,0 +1,26 @@
+'use strict';
+
+//1 - 9
+//2 - 90 - 180
+//3 - 190...
+
+function amountOfPages(summary){
+    let res = 0;
+    if (summary < 10) {
+        res = summary;
+        console.log(res);
+        return res;
+    } else if (summary > 9 && summary < 190) {
+        res  = ((summary - 9)/2) + 9;
+        console.log(res);
+        return res;      
+    } else if (summary > 189) {
+        res = ((summary - 189)/3) + 99;
+        console.log(res);
+        return res;        
+    }
+
+}
+
+
+amountOfPages(1095);
