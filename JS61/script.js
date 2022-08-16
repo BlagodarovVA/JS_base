d1593670282738edc9943186766e332056eaf43c
Valery <11w-walerik@mail.ru>
2022-08-16 13:09:20 +0300

    64 done

---
+++ JS61/script.js
@@ -0,0 +1,16 @@
+'use strict';
+
+//max int
+console.log(Number.MAX_SAFE_INTEGER);
+
+//создаем bigInt
+const bigint = 123467489798756435467897897684564889789456897n;
+
+const sameBigint = BigInt(123467489798756435467897897684564889789456897);
+
+console.log(typeof(bigint));
+
+//ошибка
+//console.log(52n +4);
+
+console.log(52n / 4n);
